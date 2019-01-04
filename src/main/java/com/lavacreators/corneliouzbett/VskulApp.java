5adef063e0c0e3fb63fc5879e6479bf01fb4068e
corneliouzbett <kbett68@gmail.com>
2019-01-04 16:23:12 +0300

    refactoring

--- src/main/java/com/lavacreators/corneliouzbett/VskulApp.java
+++ src/main/java/com/lavacreators/corneliouzbett/VskulApp.java
@@ -34,13 +34,6 @@ public class VskulApp {
         this.env = env;
     }
 
-    /**
-     * Initializes vskul.
-     * <p>
-     * Spring profiles can be configured with a program argument --spring.profiles.active=your-active-profile
-     * <p>
-     * You can find more information on how profiles work with JHipster on <a href="https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
-     */
     @PostConstruct
     public void initApplication() {
         Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
@@ -54,11 +47,6 @@ public class VskulApp {
         }
     }
 
-    /**
-     * Main method, used to run the application.
-     *
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(VskulApp.class);
         DefaultProfileUtil.addDefaultProfile(app);
